a9fb453c8c2ca6a182ab4559a09adc4aa2911ffa
dannycrews <dancrews@me.com>
2016-06-17 19:56:35 -0400

    Show two-way data binding

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 var myApp = angular.module('myApp', []);
 
 // declare a controller and inject some services
-myApp.controller('mainController', ['$scope', '$timeout', function($scope, $timeout) {
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
 
-  $scope.name = 'Dan';
+  $scope.handle = '';
 
-  $timeout(function() {
-    $scope.name = 'Everybody';
-  }, 3000);
+  $scope.lowercasehandle = function() {
+    return $filter('lowercase')($scope.handle);
+  };
 
 }]);
 
