0f7ce4eea034ce9b85027af4b00aa21588e0ac07
dannycrews <dancrews@me.com>
2016-06-17 17:51:00 -0400

    Basic setup with dependency injection.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // this is the only var in the global namespace. 
 var myApp = angular.module('myApp', []);
 
-// declare a controller
+// declare a controller and inject some services
 myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
 
   $log.info($scope);
